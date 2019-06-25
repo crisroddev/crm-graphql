2a6ceda3be3a9f306bedc5414fe4b8cab6886db5
CrisRod <crodriguez85@gmail.com>
2019-06-25 15:51:51 -0400

    get client by id

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
+import { resolvers } from './resolvers';
 
 const app = express();
 
-class Cliente {
-    constructor(id, {nombre, apellido, empresa, email}) {
-        this.id = id,
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.empresa = empresa,
-        this.email = email
-    }
-}
-
-const clientesDB = {};
-
-
-
-const root = { 
-    cliente: () => {
-        return {
-            "id": 1,
-            "nombre": "Cristian",
-            "apellido": "Rodriguez",
-            "empresa": "Mobilize",
-            "emails": "cr@gmail.com"
-            }
-         },   
-        crearCliente: ({ input }) => {
-            const id = require('crypto').randomBytes(10).toString('hex');
-            clientesDB[id] = input;
-            return new Cliente(id, input)
-
-        }
-};
-
 app.use('/graphql', graphqlHTTP({
     schema,
-    rootValue: root,
+    resolvers,
     graphiql: true
 }));
 
