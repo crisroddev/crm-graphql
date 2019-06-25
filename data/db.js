25042488a71b2892a6a52f8d3c2c8139cab8adf6
CrisRod <crodriguez85@gmail.com>
2019-06-25 17:11:17 -0400

    mongo model

---
+++ data/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://cris:Cra260385@ds149672.mlab.com:49672/clientes', { useNewUrlParser: true });
+
+const clientesSchema = new mongoose.Schema({
+    nombre: String,
+    apellido: String,
+    empresa: String,
+    email: String,
+    edad: Number,
+    tipo: String,
+    pedidos: Array
+});
+
+const Clientes = mongoose.model('clientes', clientesSchema);
+
+export { Clientes };
