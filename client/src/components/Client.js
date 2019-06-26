c98fe7992f14fea9108a9d510bbda628ef6a846f
CrisRod <crodriguez85@gmail.com>
2019-06-26 12:32:21 -0400

    Loading error data client

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Query } from 'react-apollo';
+import { CLIENTES_QUERY } from '../queries/index';
 
-const Contactos = () => {
+const Contactos = () => (
+    <Query query={CLIENTES_QUERY}>
+    {({ loading, error, data}) => {
+        if(loading) return "Cargando"
+        if(error) return `Error: ${error.message}`
+    }}
 
-}
+    </Query>
+)
 
 export default Contactos;
