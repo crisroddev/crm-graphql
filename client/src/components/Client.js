c7dc5c40ab67c5352ad04f6b17d6c3e0f489fccd
CrisRod <crodriguez85@gmail.com>
2019-06-26 12:51:36 -0400

    style

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -11,13 +11,13 @@ const Contactos = () => (
 
         return (
             <Fragment>
-                <h2 className="text-center"> Listado Clientes</h2>
-                <ul className="list-group">
+                <h2 className="text-center mt-4"> Listado Clientes</h2>
+                <ul className="list-group mt-4">
                     { data.getClientes.map(clientes => (
                         <li key={clientes.id} className="list-group-item">
                             <div className="row justify-content-between align-items-center">
                                 <div className="col-md-8 d-flex justify-content-between align-items-center">
-                                    { clientes.nombre } { clientes.apellido }
+                                    { clientes.nombre } { clientes.apellido } - {clientes.empresa}
                                 </div>
                                 <div className="col-md-4 d-flex justify-content-end">
                                     <a className="btn btn-success d-block d-md-inline-block">
