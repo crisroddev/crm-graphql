d408f9d9440860fda7fda31ffd23dce3f305f937
CrisRod <crodriguez85@gmail.com>
2019-06-26 12:16:39 -0400

    Apollo Client config

--- README.md
+++ README.md
@@ -81,3 +81,26 @@ yarn add apollo-server-express
 npx create-react-app client
 yarn add apollo-boost react-apollo graphql graphql-tag
 ```
+
+### Apollo Client Config
+```
+Client --> App.js
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'localhost:8000/graphql',
+  onError: ({ networkError, graphqlErrors}) => {
+    console.log('graphqlErrors', graphqlErrors);
+    console.log('networkError', networkError);
+  }
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+
+    </ApolloProvider>
+  );
+}
+```
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'localhost:8000/graphql',
+  onError: ({ networkError, graphqlErrors}) => {
+    console.log('graphqlErrors', graphqlErrors);
+    console.log('networkError', networkError);
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <h1>Hola</h1>
+    </ApolloProvider>
   );
 }
 
