106f166c569ca97f935f857bd5b4cadef5a517a4
CrisRod <crodriguez85@gmail.com>
2019-06-27 11:36:56 -0400

    Mutation update client

--- client/src/mutations/index.js
+++ client/src/mutations/index.js
@@ -9,3 +9,19 @@ mutation crearCliente($input: ClienteInput) {
   }
 }
 `;
+
+export const ACTUALIZAR_CLIENTE = gql `
+mutation actualizarCliente($input: ClienteInput){
+  actualizarCliente(input: $input){
+    id
+    nombre
+    apellido
+    empresa
+    emails{
+      email
+    }
+    edad
+    tipo
+  }
+}
+`;
