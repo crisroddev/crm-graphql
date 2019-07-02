30c5f08a1f8c0c8ba1a7ff11c2f48e8577289160
CrisRod <crodriguez85@gmail.com>
2019-07-02 15:41:13 -0400

    Pages on Paginator

--- client/src/components/Paginador.js
+++ client/src/components/Paginador.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class Paginador extends Component {
-    state = {  }
+    state = { 
+        paginador : {
+            paginas: Math.ceil(Number(this.props.totalClientes) / 5)
+        }
+     }
 
     render() {
 
         const { actual } = this.props;
         const btnAnterior = (actual > 1) ? <button type="button" className="btn btn-success mr-2">&laquo;Anterior</button> : '';
+
 
         return (
             <div className="mt-5 d-flex justify-content-center">
