366957b0ff75e31e4690c4a95988fbe1ad0ef3ba
CrisRod <crodriguez85@gmail.com>
2019-07-03 18:29:28 -0400

    NuevoProducto basic structure

---
+++ client/src/components/Productos/NuevoProducto.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Productos extends Component {
+        state = {  };
+    render() {
+        return (
+            <h1 className="text-center">Productos</h1>
+        );
+    }
+}
+
+export default Productos;
