b4375ac31ff9d7849cd49b9069fb64dcf255ded0
CrisRod <crodriguez85@gmail.com>
2019-07-03 18:37:36 -0400

    Form Product

--- client/src/components/Productos/NuevoProducto.js
+++ client/src/components/Productos/NuevoProducto.js
@@ -1,10 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class Productos extends Component {
         state = {  };
     render() {
         return (
-            <h1 className="text-center">Productos</h1>
+            <Fragment>
+            <h1 className="text-center">Nuevo Producto</h1>
+            <div className="row justify-content-center">
+                
+                <form 
+                    className="col-md-8">
+                    <div className="form-group">
+                        <label>Nombre:</label>
+                        <input 
+                            type="text"
+                            name="nombre" 
+                            className="form-control" 
+                            placeholder="Nombre del Producto"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Precio:</label>
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <div className="input-group-text">$</div>
+                            </div>
+                            <input 
+                                type="number" 
+                                name="precio" 
+                                className="form-control" 
+                                placeholder="Precio del Producto"/>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label>Stock:</label>
+                        <input 
+                            type="number" 
+                            name="stock" 
+                            className="form-control" 
+                            placeholder="stock del Producto" />
+                    </div>
+                    <button 
+                        type="submit" 
+                        className="btn btn-success float-right">
+                            Crear Producto
+                    </button>
+                </form>
+            </div>
+            </Fragment>
         );
     }
 }
