2493d16f90bb5ad4b609185dc4a1b1d37a4396fb
CrisRod <crodriguez85@gmail.com>
2019-07-03 16:03:21 -0400

    Productos schema on db

--- server/data/db.js
+++ server/data/db.js
@@ -4,6 +4,7 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://cris:Cra260385@ds243717.mlab.com:43717/crm', { useNewUrlParser: true });
 
+//Clientes
 const clientesSchema = new mongoose.Schema({
     nombre: String,
     apellido: String,
@@ -13,7 +14,15 @@ const clientesSchema = new mongoose.Schema({
     tipo: String,
     pedidos: Array
 });
-
 const Clientes = mongoose.model('clientes', clientesSchema);
+
+//Productos
+const productosSchema = new mongoose.Schema({
+    nombre: String,
+    precio: Number,
+    stock: Number
+
+});
+const Productos = mongoose.model('productos', productosSchema);
 
 export { Clientes };
