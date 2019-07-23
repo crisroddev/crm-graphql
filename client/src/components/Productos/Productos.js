231323f50e220eeb09388d3b053de914ad387fb3
CrisRod <crodriguez85@gmail.com>
2019-07-23 09:39:41 -0400

    Working with productos

--- client/src/components/Productos/Productos.js
+++ client/src/components/Productos/Productos.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { Query, Mutation } from 'react-apollo';
 
 export default class Products extends Component {
     render() {
         return (
-            <div>
+            <Fragment>
                 <h1 className="text-center mb-5">Productos</h1>
-            </div>
+                <Query query={CLIENTES_QUERY} pollInterval={500}>
+                {({ loading, error, data, startPolling, stopPolling}) => {
+                if(loading) return "Cargando"
+                if(error) return `Error: ${error.message}`
+                {/* console.log(data) */}
+                return (
+                        <table className="table">
+                            <head>
+                                <tr className="table-primary">
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Precio</th>
+                                    <th scope="col">Stock</th>
+                                    <th scope="col">Eliminar</th>
+                                    <th scope="col">Editar</th>
+                                </tr>
+                            </head>
+                        </table>
+                    )
+                }}
+                </Query>
+            </Fragment>
         )
     }
 }
