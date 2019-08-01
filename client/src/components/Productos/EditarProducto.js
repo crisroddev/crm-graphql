60bd382f636626a89bd70ae79ebbf30ed5ca28ae
CrisRod <crodriguez85@gmail.com>
2019-08-01 15:53:07 -0400

    form y autollenado de los datos

--- client/src/components/Productos/EditarProducto.js
+++ client/src/components/Productos/EditarProducto.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from 'react'
 import { Query } from 'react-apollo';
 import { OBTENER_PRODUCTO } from '../../queries/';
+import FormularioEditarProducto from './FormularioEditarProducto'
 
 export default class EditProduct extends Component {
     render() {
 
         const { id } = this.props.match.params
-        console.log(id)
         return (
             <Fragment>
                 <h1 className="text-center">Editar Producto</h1>
@@ -14,9 +14,15 @@ export default class EditProduct extends Component {
                     <Query query={OBTENER_PRODUCTO} variables={{id}}>
                         {({ loading, error, data, refetch }) => {
                             if(loading) return "Cargando...";
-                            if(error) return `Error ${error.message}`
-                            
-                            console.log(data);
+                            if(error) return `Error ${error.message}`;
+
+                            return (
+                                <FormularioEditarProducto
+                                    producto={data}
+                                    id={id}
+                                    refetch={refetch}
+                                />
+                            )
                         }}
 
                     </Query>
