776161160ba4ded151e703a66bb4dc80d1710928
CrisRod <crodriguez85@gmail.com>
2019-08-01 15:25:02 -0400

    editar consol.log id

--- client/src/components/Productos/EditarProducto.js
+++ client/src/components/Productos/EditarProducto.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 export default class EditProduct extends Component {
     render() {
+
+        const { id } = this.props.match.params
+        console.log(id)
         return (
             <h1 className="text-center">
                 Editar Producto
