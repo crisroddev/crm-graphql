356e5371a28fa1d55ff910fa4db77fbf1134ecbc
CrisRod <crodriguez85@gmail.com>
2019-08-01 15:41:40 -0400

    console.log data del query

--- client/src/components/Productos/EditarProducto.js
+++ client/src/components/Productos/EditarProducto.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Query } from 'react-apollo';
+import { OBTENER_PRODUCTO } from '../../queries/';
 
 export default class EditProduct extends Component {
     render() {
@@ -6,9 +8,20 @@ export default class EditProduct extends Component {
         const { id } = this.props.match.params
         console.log(id)
         return (
-            <h1 className="text-center">
-                Editar Producto
-            </h1>
+            <Fragment>
+                <h1 className="text-center">Editar Producto</h1>
+                <div className="row justify-content-center">
+                    <Query query={OBTENER_PRODUCTO} variables={{id}}>
+                        {({ loading, error, data, refetch }) => {
+                            if(loading) return "Cargando...";
+                            if(error) return `Error ${error.message}`
+                            
+                            console.log(data);
+                        }}
+
+                    </Query>
+                </div>
+            </Fragment>
         )
     }
 }
